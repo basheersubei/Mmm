341583fd371cd16a337bd2574f100bff8b397d49
Hazem <halabihazem@gmail.com>
2017-09-02 00:12:06 +0300

    Cleaned up unnecessary ANSI class instantiation and some comments

--- Mmm.py
+++ Mmm.py
@@ -5,7 +5,6 @@ import sys, tty, termios
 class Editor:
     def __init__(self):
         self._lines = []
-        self._ansi = ANSI()
         self._cursor = Cursor(1, 1)
         with open("test.txt") as lines:
             for line in lines:
@@ -26,24 +25,24 @@ class Editor:
     def handle_input(self):
         getch = _GetchUnix()
         char = getch()
-        if char == chr(17): #Ctrl+Q
+        if char == chr(17): # Ctrl+Q
             sys.stdout.write("\033[37;41mQUIT\033[39;49m\r\n")
             sys.exit(0)
-        elif char == chr(9): #Ctrl+H
+        elif char == chr(9): # Ctrl+H
             sys.stdout.write("H")
-        elif char == chr(10): #Ctrl+J
+        elif char == chr(10): # Ctrl+J
             sys.stdout.write("J")
-        elif char == chr(11): #Ctrl+K
+        elif char == chr(11): # Ctrl+K
             sys.stdout.write("K")
-        elif char == chr(12): #Ctrl+L
+        elif char == chr(12): # Ctrl+L
             sys.stdout.write("L")
                     
     def render(self):
-        self._ansi.clear_screen()
-        self._ansi.move_cursor(0, 0)
+        ANSI.clear_screen()
+        ANSI.move_cursor(0, 0)
         self._buffer.render()
-        self._ansi.move_cursor(self._cursor._row, self._cursor._col)
-        sys.stdout.flush() #Cursor move is buffered if i dont do this :/
+        ANSI.move_cursor(self._cursor._row, self._cursor._col)
+        sys.stdout.flush() # Cursor move is buffered if i dont do this :/
         
 class Buffer:
     def __init__(self, lines):
@@ -52,7 +51,7 @@ class Buffer:
     def render(self):
         for line in self._lines:
             sys.stdout.write(line + "\r\n")
-            #sys.stdout.flush() #Not required in raw mode apparently
+            #sys.stdout.flush() # Not required in raw mode apparently
             
 class Cursor:
     def __init__(self, row = 0, col = 0):
@@ -60,10 +59,10 @@ class Cursor:
         self._col = col
 
 class ANSI:
-    def clear_screen(self):
+    def clear_screen():
         sys.stdout.write("\033[2J")
 
-    def move_cursor(self, row, col):
+    def move_cursor(row, col):
         sys.stdout.write("\033[{};{}H".format(row + 1, col + 1))
 
 # Gets a single char input. Code copied from StackOverflow
